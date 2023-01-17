a5688399af61829deb17d27ee5606932b4f1fcd5
paopaol <467847281@qq.com>
2023-01-17 13:46:08 +0800

    fix(method): gen

--- lua/cpp-mode.lua
+++ lua/cpp-mode.lua
@@ -1,40 +1,115 @@
-local M = {}
+local M = {
+}
+
+local context = {
+  signature = nil
+}
+
+
+local function class_name(markdown)
+  return string.match(markdown, '// In (%w+)')
+end
+
+local function method_handler(markdown, signature)
+  local functon_name = string.match(markdown, '### [a-z-]+ `(%w+)`')
+  local before = functon_name .. '%('
+  local after = class_name(markdown) .. '::' .. functon_name .. '%('
+  return string.gsub(signature, before, after)
+end
+
+local function function_handler(markdown, signature)
+  return signature
+end
+
+local handler = {
+  ["instance-method"] = method_handler,
+  ["function"] = function_handler,
+  ["constructor"] = method_handler,
+}
+
+local function create_markdown_tmp_buf(markdown)
+  local br = vim.api.nvim_create_buf(false, true)
+
+  vim.api.nvim_buf_set_lines(br, 0, 0, true, vim.split(markdown, '\n'))
+  vim.api.nvim_buf_set_option(br, 'filetype', 'markdown')
+
+  return br
+end
+
+local function type(markdown)
+  return string.match(markdown, '### ([a-z-]+)')
+end
+
+local function signature(br)
+  local range = {}
+  vim.api.nvim_buf_call(br, function()
+    local pos = vim.fn.searchpos('```cpp', 'n')
+    pos[1]    = pos[1] + 1
+
+    range["start"] = pos
+  end)
+
+  vim.api.nvim_buf_call(br, function()
+    local pos    = vim.fn.searchpos('```$', 'n')
+    pos[1]       = pos[1] - 1
+    range["end"] = pos
+  end)
+
+  if range["start"][1] == range["end"][1] then
+    range["start"][1] = range["start"][1] - 1
+  end
+  local lines = vim.api.nvim_buf_get_lines(br, range["start"][1], range["end"][1], true)
+
+  return vim.fn.join(lines, '')
+end
+
+local function remove_keywords(signature)
+  local keywords = {
+    '^[^:]+: ',
+    '^public: ',
+    '^private: ',
+    '^protected: ',
+    '^explicit ',
+    '^static ',
+    '^inline ',
+  }
+
+  for _, key in pairs(keywords) do
+    signature = (string.gsub(signature, key, ''))
+  end
+
+  return signature
+end
 
 -- "### instance-method `Print`  \n\n---\n→ `void`  \nParameters:  \n- `int age`\n- `const char * name`\n\n---\n```cpp\n// In Printer\npublic: void Print(int age, const char *name) const\n```"
 local generate_cpp_method = function(result)
-  local contents = result["contents"].value
-  local type = string.match(contents, '### ([a-z-]+)')
-  local class_name = string.match(contents, '// In (%w+)')
-  local signature = string.match(contents, '// In .+\n(.*)\n')
-  print(type)
-  if type == 'instance-method' or type == 'static-method' or type == 'constructor' then
-    signature = (string.gsub(signature, '^[^:]+: ', ''))
-    signature = (string.gsub(signature, '^public: ', ''))
-    signature = (string.gsub(signature, '^private: ', ''))
-    signature = (string.gsub(signature, '^protected: ', ''))
-    signature = (string.gsub(signature, '^explicit ', ''))
-    signature = (string.gsub(signature, '^static ', ''))
-    local functon_name = string.match(contents, '### [a-z-]+ `(%w+)`')
-    local before = functon_name .. '%('
-    local after = class_name .. '::' .. functon_name .. '%('
-    signature = (string.gsub(signature, before, after))
-  end
-  signature = string.format("%s\n{\n}\n", signature)
-  vim.fn.setreg(0, signature)
+  local markdown = result["contents"].value
+
+  local br = create_markdown_tmp_buf(markdown)
+
+  local method = handler[type(markdown)](markdown, remove_keywords(signature(br)))
+  method = string.gsub(method, '  ', '')
+  method = string.format("%s{}", method)
+  context.signature = vim.fn.split(method, '\n')
+
+  vim.api.nvim_buf_delete(br, {})
 end
 
 M.copy_cpp_method = function()
+  context.signature = nil
   local params = vim.lsp.util.make_position_params()
   vim.lsp.buf_request(0, 'textDocument/hover', params,
     function(err, result, ctx, config)
-    generate_cpp_method(result)
-  end)
+      generate_cpp_method(result)
+    end)
 end
 
 M.paste_cpp_method = function()
-  vim.cmd([[normal! "0p]])
-  vim.cmd([[normal! jo]])
-  vim.cmd([[startinsert!]])
+  if context.signature then
+    local s = vim.fn.line('.')
+    vim.api.nvim_buf_set_lines(0, s, s, false, context.signature)
+    vim.cmd([[normal! j]])
+  end
 end
 
 
